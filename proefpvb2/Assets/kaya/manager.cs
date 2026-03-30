1814bc0abed74200a7ca9f59b98e9f1597f704ab
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2026-03-30 09:42:19 +0200

    verschillende torens plaatsen
    je kan nu uit een lijst met torens wisselen en dan die torens plaatsen

--- proefpvb2/Assets/kaya/manager.cs
+++ proefpvb2/Assets/kaya/manager.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class manager : MonoBehaviour
 {
     public int resources;
+    public KeyCode switchkey;
+
+    public gridplace gridplace;
+
+    public GameObject defenceToPlace;
+    public List<GameObject> defences;
 
     void Start()
     {
@@ -11,6 +18,16 @@ public class manager : MonoBehaviour
 
     void Update()
     {
+        defenceToPlace = defences[0];
+        gridplace.objectToPlace = defenceToPlace;
+
+        if (Input.GetKeyDown(switchkey))
+        {
+            GameObject switchObj = defences[0];
+            defences.RemoveAt(0);
+            defences.Add(switchObj);
+
+        }
         
     }
 }
